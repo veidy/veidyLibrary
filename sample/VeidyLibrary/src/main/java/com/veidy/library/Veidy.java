50c60774ce4b646c9b16575f40d985392647d003
veidy <weilong.guo@916816.com>
2016-01-05 17:25:52 +0800

    init test

---
+++ sample/VeidyLibrary/src/main/java/com/veidy/library/Veidy.java
@@ -0,0 +1,19 @@
+package com.veidy.library;
+
+import android.util.Log;
+
+/**
+ * @author : veidy
+ * @version : 2.1
+ * @Description : sample
+ * @Company : veidy
+ * @Create Date  : 2016/1/5  17:23
+ */
+public class Veidy {
+
+    private static final String TAG="Veidy";
+
+    public static void Test(){
+        Log.d(TAG,"Test");
+    }
+}
